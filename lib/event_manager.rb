05adab818f34326eaa607e25908f252051d4975e
scheals <piecoo@gmail.com>
2022-02-12 03:10:19 +0100

    Create rough phone number clean up method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,22 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(number)
+  number = number.to_s.gsub('(', '').gsub(')', '').gsub('-', '').gsub(' ', '').gsub('.', '')
+  if number.length > 11
+    '0000000000'
+  elsif number.length < 10
+    '0000000000'
+  elsif number.length == 10
+    number
+  elsif number.length == 11 && number.start_with?('1')
+    number[1..10]
+  else
+    '0000000000'
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -14,14 +29,14 @@ def legislators_by_zipcode(zip)
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -42,13 +57,20 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
+
+#   form_letter = erb_template.result(binding)
+
+#   save_thank_you_letter(id, form_letter)
+# end
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id,form_letter)
+  phone_number = clean_phone_number(row[:homephone])
+  print "#{id} #{name} #{phone_number}\n"
 end
